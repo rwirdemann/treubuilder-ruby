b7bafe6779859ec8d3e5b04d1f726d13e8256483
ralf <ralf@ralfwirdemann.de>
2015-06-05 17:11:20 +0200

    made account system nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :trustees
+  resources :trustees do
+    resources :account_systems
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
